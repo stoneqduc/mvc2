205dc565b098774e541ce7f522c24acaf1d6b626
stone <shidong_cao@tre-inc.com>
2018-03-31 15:15:46 +0900

    rename path

--- src/sample/controllers/HelloWorldController.java
+++ src/sample/controllers/HelloWorldController.java
@@ -12,7 +12,7 @@ import sample.models.HelloWorldModel;
 /**
  * Servlet implementation class HelloWorldController
  */
-@WebServlet("/HelloWorld")
+@WebServlet("/hw")
 public class HelloWorldController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
